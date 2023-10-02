aa295b4260d4d3074dcad068c5a9174b5c263c09
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-10-02 11:44:58 -0300

    V 1.0  BUG

---
+++ js/server.js
@@ -0,0 +1,11 @@
+const ShowAsk = 'Aqui é para aparecer a resposta'
+
+function CreateElementP(ShowAsk){
+  const divBELLOW = document.querySelector('div')
+  const pergunta = document.querySelector('h1')
+  pergunta.addEventListener( 'click', putASKhtml(ShowAsk))
+  function putASKhtml( ShowAsk ){
+    divBELLOW.innerHTML = `<p>${ShowAsk}</p>`
+  }
+  console.log("\n\n executado função CreateElementP\n--\n")
+}
