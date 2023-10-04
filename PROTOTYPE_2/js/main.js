3076c4fd34fdc029a5527d2591ef41f5b74c896a
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-10-04 13:25:05 -0300

    bug4

--- PROTOTYPE_2/js/main.js
+++ PROTOTYPE_2/js/main.js
@@ -1,17 +1,32 @@
-const h1 = document.querySelector("h1")
+const hh1 = document.querySelector("h1")
+console.log("o que tem dentro de h1 antes ? \n",hh1)
+
+const h1s = document.querySelectorAll("h1")
+console.log("\n E o que tem dentro de h1 agora ? \n",h1s)
+
+let count = 1 ;
 //refatorando 
-h1.addEventListener("click" , functions)
+h1s.forEach(   (Each_h1) =>           Each_h1.addEventListener("click" , functions)   )
 
 function functions(){
   console.log(" executando functions ")
   const ask = document.getElementById('ask')
   ask.classList.toggle('div')
   animation()
+  
 }
 function animation(){
+    console.log("\n\n",count)
+    let ValueForToAnimate = 0
+    ValueForToAnimate = count%2 === 0 ? ValueForToAnimate = 0 : ValueForToAnimate = 180
+
    // document.querySelector('iconOPENask img').style.transitionDuration = '0.5s'
-    document.querySelector('.iconOPENask img').style.transform = 'rotate(180deg)'
+    document.querySelector('.iconOPENask img').style.transform = `rotate(${ValueForToAnimate}deg)`
+    document.querySelector('.iconOPENask img').style.transitionDuration = '0.5s'
     console.log("Executado o animation")
+
+    count++
+    console.log(count, "\n\n")
 }
 
 //document.getElementById(i).style.WebkitTransitionDuration="1s"; dure 1 segundo
