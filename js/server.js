3ce56f02ba25195bcb698ad9a95410495ece4167
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-10-04 10:10:28 -0300

    Bug one

--- js/server.js
+++ js/server.js
@@ -3,9 +3,10 @@ const ShowAsk = 'Aqui é para aparecer a resposta'
 function CreateElementP(ShowAsk){
   const divBELLOW = document.querySelector('div')
   const pergunta = document.querySelector('h1')
-  pergunta.addEventListener( 'click', putASKhtml(ShowAsk))
+  pergunta.addEventListener( "click", putASKhtml(ShowAsk))
+  
   function putASKhtml( ShowAsk ){
-    divBELLOW.innerHTML = `<p>${ShowAsk}</p>`
+   divBELLOW.innerHTML = `<p>${ShowAsk}</p>`
   }
   console.log("\n\n executado função CreateElementP\n--\n")
 }
