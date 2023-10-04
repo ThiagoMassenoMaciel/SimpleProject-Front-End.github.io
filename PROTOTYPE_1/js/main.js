1d045a2a6da655a4a9b39c1d3720f4ae1b1cc521
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-10-04 10:27:47 -0300

    Prototype 1  working

---
+++ PROTOTYPE_1/js/main.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("h1")
+h1.addEventListener("click" , () => document.querySelector('div').classList.toggle('div') )
+
+// expected 
+/*             __> put
+After click __/     or       the display none from div 
+              \__> get out 
+
+
+*/
