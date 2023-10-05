f8830ab82080350a3f3ff5ad168b9547e76480a9
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-10-04 21:14:29 -0300

    bug4 solved and refatored

--- PROTOTYPE_2/js/main.js
+++ PROTOTYPE_2/js/main.js
@@ -1,33 +1,82 @@
-const hh1 = document.querySelector("h1")
-console.log("o que tem dentro de h1 antes ? \n",hh1)
 
-const h1s = document.querySelectorAll("h1")
-console.log("\n E o que tem dentro de h1 agora ? \n",h1s)
+const p1 = document.querySelector("#p1")
+const p2 = document.querySelector("#p2")
+
+const ask1 = document.getElementById('ask1')
+const ask2 = document.getElementById('ask2')
+
+console.log(p1)
+console.log(ask1)
+
+console.log(p2)
+console.log(ask2)
 
 let count = 1 ;
-//refatorando 
-h1s.forEach(   (Each_h1) =>           Each_h1.addEventListener("click" , functions)   )
+//refatorar este codigo
+p1.addEventListener("click" , ()=> functions(ask1,p1))   
+p2.addEventListener("click" , ()=> functions(ask2,p2))   
 
-function functions(){
+function functions(ask,p){
   console.log(" executando functions ")
-  const ask = document.getElementById('ask')
+  ask.classList.toggle('div')
+  animation(p)
+  
+}
+
+/*    este n deu certo pois o event listener ja esta executando automaticamente mesmo quando eu n clico na h1 e 
+function functions(id){
+  console.log(" executando functions ")
+  const ask = document.getElementById(`${id}`)
   ask.classList.toggle('div')
   animation()
   
 }
+*/
+
+/*
+function functions(id){
+  console.log(" executando functions ")
+  const ask = document.getElementById(id)
+  ask.classList.toggle('div')
+  animation()
+  
+}
+*/
+/*
+function functions(ask){
+  console.log(" executando functions ")
+  const ask = document.getElementById('ask1')
+  ask.classList.toggle('div')
+  animation()
+  
+}
+*/
+function animation(p){
+  console.log(count)
+  let ValueForToAnimate = 0
+  ValueForToAnimate = count%2 === 0 ? ValueForToAnimate = 0 : ValueForToAnimate = 180
+  
+ // document.querySelector('iconOPENask img').style.transitionDuration = '0.5s'
+  p.querySelector('img').style.transform = `rotate(${ValueForToAnimate}deg)`
+  p.querySelector('img').style.transitionDuration = '0.5s'
+  console.log("Executado o animation")
+
+  count++
+  console.log(count)
+}
+/*
 function animation(){
-    console.log("\n\n",count)
+    console.log(count)
     let ValueForToAnimate = 0
     ValueForToAnimate = count%2 === 0 ? ValueForToAnimate = 0 : ValueForToAnimate = 180
-
+    
    // document.querySelector('iconOPENask img').style.transitionDuration = '0.5s'
     document.querySelector('.iconOPENask img').style.transform = `rotate(${ValueForToAnimate}deg)`
     document.querySelector('.iconOPENask img').style.transitionDuration = '0.5s'
     console.log("Executado o animation")
 
     count++
-    console.log(count, "\n\n")
+    console.log(count)
 }
-
-//document.getElementById(i).style.WebkitTransitionDuration="1s"; dure 1 segundo
-//document.getElementById(i).style.webkitTransform = 'rotate(40deg)'; rode 40 graus
+*/
+// criar uma call function diferente para  cada pergunta pois chamar as mesma função passando parametros(nome do id que contem a resposta da pergunta clicada) diferentes não deu certo 
